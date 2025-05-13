614324a319a4eaf5d519ae082771ffc73485814e
Rezakori <66035933+RonaldoK618@users.noreply.github.com>
2025-05-13 10:13:31 -0600

    Added Week 2 Assignment Contents

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <div>Ronaldo K.</div>
+            <Link href="https://github.com/RonaldoK618/cprg306-assignments">Ronaldo's Repository</Link>
+        </main>
+    );
+}
